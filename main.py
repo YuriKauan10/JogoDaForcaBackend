36970acc783f637451fa031c04772008dc651f5e
YuriKauan10 <yurrikauan@gmail.com>
2025-05-02 15:18:02 -0300

    tests

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-palavra = 'yurizin'
-sugestao = 'i'
 
+while True:
+    palavra = 'yurizin'
+    sugestao = input("Digite sua sugestão: ")
 
-nova = ''
+    nova = ''
 
-for i in range(len(palavra)):
-    if sugestao == palavra[i]:
-        nova += sugestao + " "
-        contIndice = i
-    else:
-        nova += "_ "
-
-print(nova)
+    for i in range(len(palavra)):
+        if sugestao == palavra[i]:
+            nova += sugestao + " "
+            contIndice = i
+        else:
+            nova += "_ "
+    print(nova)
+    if sugestao == palavra:
+        print("Palavra correta!")
+        break
 
 
 
